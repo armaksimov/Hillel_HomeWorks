d87130372c2fa5d88b50b4392a2a68b416a60876
armaksimov <42908929+armaksimov@users.noreply.github.com>
2019-12-17 23:11:42 +0300

    Delete Recursion.java

--- Recursion.java
+++
@@ -1,7 +0,0 @@
-public class Recursion {
-    public static void main(String[] args) {
-        RecursiveOutput rec = new RecursiveOutput();
-        System.out.println("Sum is: " + rec.recursiveSum(10));
-        System.out.println("Sum of nums digit is: " + rec.recursiveNumDigitsSum(123456));
-    }
-}
