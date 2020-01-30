604b03c1cf2790ea88c6760be7fcd18e8a1a05c2
armaksimov <42908929+armaksimov@users.noreply.github.com>
2020-01-30 19:12:20 +0300

    Create Hatchback.java

---
+++ com/hillel/Taxipark4/classes/Hatchback.java
@@ -0,0 +1,14 @@
+package com.hillel.Taxipark4.classes;
+
+public class Hatchback extends Car{
+    String className;
+    public Hatchback(String mark, String model, int fuelConsumption, int speed, int price) {
+        super(mark, model, fuelConsumption, speed, price);
+        this.className = getClass().getName();
+    }
+
+    @Override
+    public String toString() {
+        return getMark() + "," + getModel() + "," + getFuelConsumption() + "," + getSpeed() + "," + getPrice() + "," + getClass().getName() + "; \r\n";
+    }
+}
