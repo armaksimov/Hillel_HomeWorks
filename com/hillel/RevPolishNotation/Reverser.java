6256772fee15c1cc5201b2e03386d42717c92418
armaksimov <42908929+armaksimov@users.noreply.github.com>
2020-02-03 22:33:12 +0300

    Update Reverser.java

--- com/hillel/RevPolishNotation/Reverser.java
+++ com/hillel/RevPolishNotation/Reverser.java
@@ -7,6 +7,11 @@ public class Reverser {
         Stack<String> stack = new Stack();
         String eqElements[] = equation.split(" ");
         int numOfOperators = (eqElements.length - 1) / 2;
+        for (int i = 0; i < eqElements.length; i++) {
+            if(eqElements[i].equals("(")){
+                numOfOperators--;
+            }
+        }
         String operators[] = new String[numOfOperators];
         Integer operands[] = new Integer[numOfOperators + 1];
         int c = 0;
@@ -16,9 +21,12 @@ public class Reverser {
                 operators[c] = eqElements[i];
                 c++;
             }
-            else{
+            else if(isNum(eqElements[i])){
                 operands[oC] = Integer.parseInt(eqElements[i]);
                 oC++;
+            }
+            if(eqElements[i].equals(")")){
+                continue;
             }
         }
         // pushing first operand
@@ -55,9 +63,10 @@ public class Reverser {
         Object reversed[] = stack.toArray();
 
 //        for polish notation
-//        for (int i = reversed.length - 1; i >= 0; i--) {
-//           System.out.print(reversed[i] + " ");
-//        }
+        for (int i = reversed.length - 1; i >= 0; i--) {
+           System.out.print(reversed[i] + " ");
+        }
+        System.out.println();
 
         String newEq = "";
         for (int i = 0; i < reversed.length; i++) {
@@ -66,5 +75,12 @@ public class Reverser {
         newEq += "= " + res;
         return newEq;
     }
-
+    public static boolean isNum(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
 }
