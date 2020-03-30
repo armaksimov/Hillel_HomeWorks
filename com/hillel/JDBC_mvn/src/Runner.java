c1af33b6d040d1e9f5c8478d75001c2cbe461202
Artem Maksymov <42908929+armaksimov@users.noreply.github.com>
2020-03-30 21:12:05 +0300

    Update Runner.java

--- com/hillel/JDBC_mvn/src/Runner.java
+++ com/hillel/JDBC_mvn/src/Runner.java
@@ -1,10 +1,14 @@
 package com.hillel.databases;
 
+import java.sql.SQLException;
 import java.text.ParseException;
 
 public class Runner {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException, SQLException {
         filmLib lib = new filmLib();
-        System.out.println(lib.getNewFilms(2020));
+        System.out.println(lib.getNewFilms());
+        System.out.println(lib.actor_filmdirector());
+        System.out.println(lib.getOldFilms(2002));
+        System.out.println(lib.actorsFilmedInMoreFilms(3));
     }
 }
